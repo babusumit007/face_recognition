642ded2e3ff509f1863166457c6cb594f6de1a64
GUR34571 <GUR34571@GURWINF113281.ad.aricent.com>
2019-02-06 09:58:29 +0530

    added first commit for fr

--- Demo.py
+++ Demo.py
@@ -4,32 +4,9 @@ Created on Jan 30, 2019
 @author: Sumit
 '''
 import face_recognition as fr 
-import cv2
-
 
 def video():
-    cam = fr.
-    while(True):
-        gray = face_recognition.load_image_Camera(cam)
-        faces = face_recognition.face_Detection(gray)
-        print(len(faces))
-        for(x,y,w,h) in faces:
-            cv2.rectangle(gray,(x,y), (x+w, y+h), (0,0,255), 2)
-        face_recognition.show_image_file(gray,'Camera')
-        if cv2.waitKey(50) & 0xFF == ord('q'):
-            break
-    face_recognition.realease_Camera(cam)
-    cv2.destroyAllWindows()
-    face_recognition.image(faces)
-def main():
-    image=face_recognition.load_image_file('image.JPG')
-    
-    face_recognition.show_image_file(image,'windows')
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-def face():
-    pass
+    camera=fr.camera(1)
     
 
 if __name__ == '__main__':
