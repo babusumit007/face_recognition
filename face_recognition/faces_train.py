695be04b9ec7f99582d363dd468ba841503ca1d8
GUR34571 <GUR34571@GURWINF113281.ad.aricent.com>
2019-02-07 12:41:17 +0530

    fr

--- face_recognition/faces_train.py
+++ face_recognition/faces_train.py
@@ -4,8 +4,17 @@ Created on Feb 6, 2019
 @author: Sumit
 '''
 import os
+import pickle
+from PIL import Image
+import numpy as np
+import cv2
 
 BASE_DIR = os.path.dirname(os.path.abspath(__name__))
+
+face_cascade = cv2.CascadeClassifier('.//face_recognition//Tools//haarcascade_frontalface_default.xml')
+
+y_lables = []
+x_train = []
 
 def create(images):
     image_dir = os.path.join(BASE_DIR, images)
@@ -14,4 +23,27 @@ def create(images):
             if file.endswith("png") or ("jpg"):
                 path = os.path.join(root, file)
                 lable = os.path.basename(root).replace(" ","-" ).lower()
-                print(lable, path)
+                #print(lable, path)
+                #y_lables.append(lable)
+                #x_train.append(path)
+                pil_image = Image.open(path).convert("L")#grayscale
+                image_array = np.array(pil_image)
+                #print(image_array)
+                faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.55, minNeighbors=5, minSize=(40,40))
+                for (x,y,w,h) in faces:
+                    cv2.rectangle(image_array,(x,y),(x+w,y+h),(255,0,0),2)
+                    roi_color = image_array[y:y+h, x:x+w]
+                    print(roi_color)
+                    x_train.append(roi_color)
+                    
+                    
+                    
+                    
+                    
+                    #while True:
+                    #    cv2.imshow('temp',image_array)
+                    #    if cv2.waitKey(50) & 0xFF == ord('q'):
+                    #        break
+ 
+                
+#print(x_train)                
